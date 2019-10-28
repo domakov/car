88d9b0bb68dd6057fa18e8059df33f24247f1eb9
ASEKEEEE <aslan.10.11.02@inbox.ru>
2019-10-28 14:28:01 +0500

    ASEKE
    EASYYYYYYYYYYYYYYYYYY

--- index.php
+++ index.php
@@ -7,43 +7,9 @@
 </head>
 <body class="page">
 	<!--BEGIN HEADER-->
-		<div id="header">
-			<div class="top_info">
-				<div class="logo">
-					<a href="#">Auto<span>Dealer</span></a>
-				</div>
-				<div class="header_contacts">
-					<div class="phone">+1 (800) 455-55-95</div>
-					<div>WinterJuice, LLC, 1875 South Grant Street, Suite 680, San Mateo, CA 94402</div>
-				</div>
-				<div class="socials">
-					<a href="#"><img src="images/fb_icon.png" alt=""></a>
-					<a href="#"><img src="images/twitter_icon.png" alt=""></a>
-					<a href="#"><img src="images/in_icon.png" alt=""></a>
-				</div>
-			</div>
-			<div class="bg_navigation">
-				<div class="navigation_wrapper">
-					<div id="navigation">
-						<span>Home</span>
-						<ul>
-							<li class="current"><a href="#">Home</a></li>
-							<li><a href="#">About Us</a></li>
-							<li><a href="#">Blog</a></li>
-							<li><a href="#">News</a></li>
-							<li><a href="#">For Salers</a></li>
-							<li><a href="#">Contacts</a></li>
-						</ul>
-					</div>
-					<div id="search_form">
-						<form method="get" action="#">
-							<input type="text" onblur="if(this.value=='') this.value='Search on site';" onfocus="if(this.value=='Search on site') this.value='';" value="Search on site" class="txb_search"/>
-							<input type="submit" value="Search" class="btn_search"/>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
+		<?
+		include ('includes/header.php');
+		?>
 	<!--EOF HEADER-->
 	<!--BEGIN CONTENT-->
 		<div id="content">
