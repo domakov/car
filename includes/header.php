148c86f130be96add3cfb7a4cba5788c444c566f
domakov <domakov91@gmail.com>
2019-11-02 08:50:13 +0500

    Лави наваявална абнавления
    Мы тут кароче лого ссилка добавили

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <div id="header">
 			<div class="top_info">
 				<div class="logo">
-					<a href="#"><img src="images/logo_logo.png" alt=""></a>
+					<a href="index.php"><img src="images/logo_logo.png" alt=""></a>
 				</div>
 				<div class="header_contacts">
 					<div class="phone">+1 (800) 455-55-95</div>
